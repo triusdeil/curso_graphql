45a0b3e04a6f8324cfde3a57f8bfc5f70932d41e
Triusdeil <34854558+triusdeil@users.noreply.github.com>
2021-07-17 03:10:54 -0400

    cargando en express graphql

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.json({
+        message: 'Hello World'
+    })
+})
+
+const schema = {}
+
+app.use('/graphql', graphqlHTTP({
+    graphiql:true,
+    schema: schema
+}))
+
+
+app.listen(3000,() => console.log('server on port 3000'))
